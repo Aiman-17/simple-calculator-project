13c263c23b9ef9361a095ffb6f5e0e8e42304b11
Aiman Syeda <160509703+Aiman-17@users.noreply.github.com>
2024-03-19 06:31:29 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+
+const Solution = await inquirer.prompt([
+  { message: "enter a first number=", type: "number", name: "firstNumber" },
+  { message: "enter a second number=", type: "number", name: "secondNumber" },
+  { message: "enter a third number=", type: "number", name: "thirdNumber" },
+  { message: "enter a fourth number=", type: "number", name: "fourthNumber" },
+
+  {
+    message: "select one of the operator to perform operation",
+    type: "list",
+    name: "calculator",
+    choices: ["multiplication", "division", "subtraction", "addition"],
+  },
+]);
+
+if (Solution.calculator === "division") {
+  console.log(
+    Solution.firstNumber /
+      Solution.secondNumber /
+      Solution.thirdNumber /
+      Solution.fourthNumber
+  );
+} else if (Solution.calculator === "multiplication") {
+    console.log(
+        Solution.firstNumber *
+          Solution.secondNumber *
+          Solution.thirdNumber *
+          Solution.fourthNumber
+      ); 
+    } else if (Solution.calculator === "addition") {
+        console.log (
+            Solution.firstNumber +
+              Solution.secondNumber +
+              Solution.thirdNumber +
+              Solution.fourthNumber
+          );
+        } else if (Solution.calculator === "subtraction") {
+            console.log (
+                Solution.firstNumber -
+                  Solution.secondNumber -
+                  Solution.thirdNumber -
+                  Solution.fourthNumber
+              );
+            }
+
